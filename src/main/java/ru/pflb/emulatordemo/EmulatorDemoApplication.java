9f294083e8a63ffba22505d3a4f3ce5deff2b85a
ye <evgeny.golovnya@x5.ru>
2020-09-19 20:48:59 +0300

    initial

---
+++ src/main/java/ru/pflb/emulatordemo/EmulatorDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.pflb.emulatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmulatorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmulatorDemoApplication.class, args);
+    }
+
+}
